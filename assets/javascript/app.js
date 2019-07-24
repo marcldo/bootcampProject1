c194b43f3b8bda38f78368920dd64c9c0597bc29
Vicky Law <vicky.law1217@gmail.com>
2019-07-24 10:36:05 -0400

    added commemts on HTML and CSS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,17 @@
-$(document).ready(function () {
-    
-    $("#submit").on("click", function () {
-        $.ajax({
-            url: "https://developers.zomato.com/api/v2.1/cities?q=toronto",
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('user-key',
-                    '800b518a5824533907d36cfa8844ff50 ');
-            }
-        }).then(function (response) {
-            console.log(response);
-        });
+//$(document).ready(function () {
+
+$("#submit").on("click", function () {
+
+    $.ajax({
+        url: "https://developers.zomato.com/api/v2.1/cities?q=toronto",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('user-key',
+                '800b518a5824533907d36cfa8844ff50');
+        }
+    }).then(function (response) {
+        console.log(response);
     });
+});
 
 
 
@@ -19,5 +20,5 @@ $(document).ready(function () {
 
 
 
-    
-    });
+
+   // });
