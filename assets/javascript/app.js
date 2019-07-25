f5babce5838f26e08408d1d6f6277423483b1239
Sachin Jhaveri <sachinjaveri.2010@gmail.com>
2019-07-24 21:48:43 -0400

    Data attributes added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,8 @@ $("#submit").on("click", function() {
     method: "GET",
     beforeSend: function(xhr) {
       xhr.setRequestHeader("user-key", "800b518a5824533907d36cfa8844ff50 ");
-    }
+    },
+    cache: false
   }).then(function(response) {
     var op = "<table>";
 
@@ -17,20 +18,34 @@ $("#submit").on("click", function() {
     console.log(response.country_flag_url);
     for (i = 0; i < response.location_suggestions.length; i++) {
       var result = response.location_suggestions[i];
-      op +=
-        "<tr> <td>" +
-        result.name +
-        "</td><td>" +
-        result.country_name +
-        "</td><td><img src=" +
-        result.country_flag_url +
-        "></img></td>";
+      op += `<tr class="location" data-name="${result.name}"> <td class="${i}">
+        ${result.name} 
+        </td><td class="${i}"> 
+        ${result.country_name} 
+        </td><td><img class="${i}" src= 
+        ${result.country_flag_url} 
+        "></img></td>`;
     }
 
     op += "</table>";
     document.getElementById("datainsert").innerHTML = op;
+    setApiData(response);
   });
 });
-$("#datainsert").on("click", function(e) {
-  console.log(e.target);
+
+$("#datainsert").on("click", ".location", function(e) {
+  console.log("hey, location got clicked", $(this).data("name"));
 });
+
+function setApiData(response) {
+  $("#datainsert").on("click", function(e) {
+    for (var i = 0; i < response.location_suggestions.length; i++) {
+      if (e.target.attributes.class.nodeValue == i) {
+        console.log(response.location_suggestions[i]);
+      }
+    }
+  });
+  // $("#submit").on("click", function() {
+  //   window.location.reload();
+  // });
+}
