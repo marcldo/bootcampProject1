afda8991ceab05fe84832a2e9a08e918a714936f
Vicky Law <vicky.law1217@gmail.com>
2019-07-26 15:56:23 -0400

    font larger and flags align

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,28 +11,30 @@ $("#submit").on("click", function () {
     }
   }).then(function (response) {
     var op = "<table>";
-
-    op += "<tr><th> CITY</th></tr>";
+    console.log(response);
+    op += "<tr><th > CITY </th></tr>";
+    
+    
     for (i = 0; i < response.location_suggestions.length; i++) {
       var result = response.location_suggestions[i];
       console.log(result);
-      op += `<tr> <td class="location" data-name="${result.name}" data-id="${
-        result.country_id
-        }" data-state="${result.state_code}" > 
-        ${result.name} 
-        <img src= 
-        ${result.country_flag_url} 
-        "></img></td>`;
+      op += `<tr> <td class="location" data-name="${result.name}" data-id="${result.country_id}" data-state="${result.state_code}" 
+        data-city-id="${result.id}">  
+       <img src="${result.country_flag_url}" align="left"></img>&nbsp; &nbsp;&nbsp; &nbsp; ${result.name} </td>`;
     }
 
     op += "</table>";
     document.getElementById("datainsert").innerHTML = op;
+    
   });
 });
-$("#datainsert").on("click", function (e) {
 
-  console.log(e.target);
-  var tableCity = $(e.target).text();
+
+$("#datainsert").on("click", ".location", function (e) {
+  console.log("hey, location got clicked", $(this).data("name"));
+  // console.log("hey, location got clicked", $(this).data("id"));
+
+  var tableCity = $(this).data("name");
   console.log(tableCity);
   var city = tableCity.substring(0, tableCity.indexOf(","));
   console.log(city);
@@ -44,25 +46,49 @@ $("#datainsert").on("click", function (e) {
     "&stateCode=" +
     state;
 
-
   $.ajax({
     url: tQueryURL,
-    method: "GET",
+    method: "GET"
   }).then(function (response) {
     console.log(response);
-    "<tr><th> CITY</th></tr>";
+    ("<tr><th> CITY</th></tr>");
     var eventsTable = "<table>";
-    eventsTable += "<tr><th> EVENTS </th></tr>"
+    eventsTable += "<tr><th> EVENTS </th></tr>";
     for (var i = 0; i < 10; i++) {
       var result = response._embedded.events[i];
-      eventsTable += `<tr><td><a href=${result.url}>${result.name} &nbsp; &nbsp;  ${result._embedded.venues[0].name} &nbsp; &nbsp; ${result.dates.start.localDate}</a></td></tr>`
-console.log(result.url)
+      eventsTable += `<tr><td><a href=${result.url} target= "_blank">${result.images[0]} ${
+        result.name
+        } &nbsp; &nbsp;  ${result._embedded.venues[0].name} &nbsp; &nbsp; ${
+        result.dates.start.localDate
+        }</a></td></tr>`;
+      console.log(result.url);
     }
     eventsTable += "</table>";
     document.getElementById("eventsData").innerHTML = eventsTable;
   });
 
+  var zQueryURL = "https://developers.zomato.com/api/v2.1/collections?city_id=" + $(this).data("city-id");
+
+  $.ajax({
+    url: zQueryURL,
+    method: "GET",
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader("user-key", "800b518a5824533907d36cfa8844ff50 ");
+    }
+  }).then(function (response) {
+    console.log(response);
+    console.log(response.collections[0]);
+    var collectionsTable = "<table>";
+    collectionsTable += "<tr><th> Restaurants </th></tr>";
+    for (var i = 0; i < 10; i++) {
+      var result = response.collections[i].collection;
+      collectionsTable += `<tr><td><a href=${result.url} target = "_blank">${
+        result.description
+        }</a></td></tr>`;
+
+    }
+    collectionsTable += "</table>";
+    document.getElementById("collectionsData").innerHTML = collectionsTable;
+  });
 });
-/*$("#eventsData").on("click", function (e) {
-   console.log()
- })*/
+
